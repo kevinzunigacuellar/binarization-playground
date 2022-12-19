eb9885df0c7f5b739dade757782fecb1e008fce9
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-12-18 23:01:40 -0500

    binarized image initial setup

--- src/components/uploadFiles.jsx
+++ src/components/uploadFiles.jsx
@@ -5,23 +5,43 @@ export default function Form() {
   let canvasRef;
 
   createEffect(() => {
+    // update the canvas when the imagePreviewURL changes
     if (store.imagePreviewURL) {
       const ctx = canvasRef.getContext("2d");
       const img = new Image();
+      img.src = store.imagePreviewURL;
       img.onload = function() {
+        // set the canvas size to the image size
         canvasRef.width = img.width;
         canvasRef.height = img.height;
+        // draw the image on the canvas
         ctx.drawImage(img, 0, 0);
+        // save the image data in the store
+        setStore("image", ctx.getImageData(0,0,canvasRef.width, canvasRef.height));
       }
-      img.src = store.imagePreviewURL;
     }
-    console.log(store.algorithm)
   })
 
+  const handleClick = async () => {
+    // loads binarization algorithms
+    const algo = await store.algorithm.initialize();
+    // create a new image from the canvas
+    const image = await new store.algorithm.Image(store.image.width, store.image.height, store.image.data);
+    // create a binary image from the canvas
+    const binImage = store.algorithm.Binarization.toBinary(algo.OTSU, image, { window: 26, k: 0.10 });
+    // draw the binary image on the canvas
+    binImage.draw(canvasRef);
+    // free the memory
+    image.free();
+    binImage.free();
+  }
+
   const uploadFile = async (e) => {
+    // get the image from the input
     const image = e.target.files[0];
+    // create a preview URL
     const imagePreview = URL.createObjectURL(image);
-    setStore("image", image);
+    // save the preview URL in the store
     setStore("imagePreviewURL", imagePreview);
   }
   
@@ -34,6 +54,7 @@ export default function Form() {
     <Show when={store.imagePreviewURL}>
       <canvas ref={canvasRef} style={{"max-width": "400px"}}></canvas>
     </Show>
+    <button onClick={handleClick}>binarize</button>
     </div>
   )
 }
