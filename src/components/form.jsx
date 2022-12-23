d205815ea642e619492151ca1d01c8dcb320deec
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-12-22 22:07:46 -0500

    style fix

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -215,7 +215,7 @@ export default function Form() {
           />
         </div>
         <button
-          class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300"
+          class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300"
           onClick={handleDownload}
         >
           <span class="relative w-full px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
