cf50210cc6cb4c79f4603d1537167c2ea5a75a2e
Kevin Zuniga Cuellar <46791833+kevinzunigacuellar@users.noreply.github.com>
2023-01-06 14:43:14 -0500

    add select with ui lib (#5)

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,7 +1,6 @@
 import { store, setStore } from "@scripts/store";
-import { Select } from "@thisbeyond/solid-select";
-import { Accessor, Show } from "solid-js";
-import "./select.css";
+import { Accessor, Show, For } from "solid-js";
+import { Select } from "@kobalte/core";
 
 async function binarize(canvas: Accessor<HTMLCanvasElement | undefined>) {
   const start = performance.now();
@@ -56,19 +55,65 @@ export default function Tools({ canvas }: ToolsProps) {
 
   return (
     <div class="w-full sm:w-72 h-full p-4 sm:border-r border-zinc-700 bg-zinc-800 flex flex-col gap-2 order-2 sm:order-none">
-      <label
-        for="select-input2"
-        class="block text-sm font-medium text-zinc-400"
-      >
-        Select an algorithm
-      </label>
-      <Select
-        options={Object.keys(store.binarizationAlgorithms)}
-        class="custom"
-        placeholder="Choose an option"
-        id="select-input2"
-        onChange={handleSelect}
-      />
+      <Select onValueChange={handleSelect}>
+        <Select.Label class="text-zinc-400 text-sm font-medium">
+          Select an algorithm
+        </Select.Label>
+        <Select.Trigger
+          aria-label="binarization algorithms"
+          class="bg-zinc-800 border text-zinc-400 border-zinc-700 rounded-lg flex text-sm justify-between items-center p-2 outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          <Select.Value placeholder="Select an option" />
+          <Select.Icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </Select.Icon>
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Content class="bg-zinc-800 border-zinc-700 border rounded-lg py-1">
+            <Select.Listbox class="">
+              <For each={Object.keys(store.binarizationAlgorithms)}>
+                {(algorithm) => (
+                  <Select.Item
+                    value={algorithm}
+                    class="flex py-1 px-2 text-zinc-400 text-sm justify-between items-center outline-none focus:bg-zinc-900 "
+                  >
+                    <Select.ItemLabel>{algorithm}</Select.ItemLabel>
+                    <Select.ItemIndicator>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        class="w-5 h-5 mr-1 text-emerald-400"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M4.5 12.75l6 6 9-13.5"
+                        />
+                      </svg>
+                    </Select.ItemIndicator>
+                  </Select.Item>
+                )}
+              </For>
+            </Select.Listbox>
+          </Select.Content>
+        </Select.Portal>
+      </Select>
       <Show when={store.selectedAlgorithm.parameters.window}>
         <label
           for="window-size"
@@ -82,7 +127,7 @@ export default function Tools({ canvas }: ToolsProps) {
           type="number"
           step={5}
           min={0}
-          class="bg-zinc-700/50 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border-0 appearance-none"
+          class="bg-zinc-800 border-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border"
           onInput={async (e) => {
             const windowSize = e.currentTarget.valueAsNumber;
             setStore("parameters", "window", windowSize);
@@ -101,7 +146,7 @@ export default function Tools({ canvas }: ToolsProps) {
           type="number"
           min={0}
           step={10}
-          class="bg-zinc-700/50 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border-0 appearance-none"
+          class="bg-zinc-800 border-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border"
           onInput={async (e) => {
             const threshold = e.currentTarget.valueAsNumber;
             setStore("parameters", "threshold", threshold);
@@ -123,7 +168,7 @@ export default function Tools({ canvas }: ToolsProps) {
           type="number"
           min={0}
           step={5}
-          class="bg-zinc-700/50 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border-0 appearance-none"
+          class="bg-zinc-800 border-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border"
           onInput={async (e) => {
             const contrastLimit = e.currentTarget.valueAsNumber;
             setStore("parameters", "contrast-limit", contrastLimit);
@@ -142,7 +187,7 @@ export default function Tools({ canvas }: ToolsProps) {
           type="number"
           step={0.05}
           min={0}
-          class="bg-zinc-700/50 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2 border-0 appearance-none"
+          class="bg-zinc-800 border-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-zinc-400 text-sm rounded-lg block w-full p-2"
           onInput={async (e) => {
             const k = e.currentTarget.valueAsNumber;
             setStore("parameters", "k", k);
